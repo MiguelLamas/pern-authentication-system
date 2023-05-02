fea22411d0d1190b51657cf9851f015d5c0fbaf3
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-02 15:43:27 +0100

    Finished Register Function and Tested Registration Functions

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,18 +1,34 @@
 const db = require('../db');
+const {hash} = require("bcryptjs"); // hash password
 
 exports.getUsers = async (req, res) => {
     try {
-    const { rows } = await db.query('SELECT * FROM users');
-    console.log(rows);
+    const { rows } = await db.query('SELECT user_id, email FROM users');
+    
+    return res.status(200).json({
+        success: true,
+        users: rows,
+    })
     } catch (error) {
         console.log(error.message);
     }
 }
 
 exports.register = async (req, res) => {
+    const { email, password } = req.body;
     try {
-    console.log('validation passed');
+        const hashedPassword = await hash(password, 12)
+        await db.query('INSERT INTO users(email, password) VALUES($1, $2)', [email, hashedPassword]
+        );
+        return res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+        });
+        
     } catch (error) {
         console.log(error.message);
+        return res.status(500).json({
+            error: error.message,
+        })
     }
 }
