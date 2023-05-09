1e158697308413d4ec510dc6cb6bd872c74f0fb2
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-09 15:53:39 +0100

    Created Navbar component and then Layout component with Navbar imported so that Navbar renders on all children layouts. Replaced divs in different pages components with Layout

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 const Register = () => {
   return (
-    <div>
+    <Layout>
       <h1>Register</h1>
-    </div>
+    </Layout>
   )
 }
 
