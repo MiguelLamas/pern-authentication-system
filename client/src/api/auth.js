87dd06664beaa1c2a304e56819c729fafd51b564
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-10 00:02:04 +0100

    Created auth API functions to communicate with the backend using axios

---
+++ client/src/api/auth.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+/* we are dealing with cookies so we will use axios.defaults.withCredentials = true
+so we can send cookies back to the server, and check the token we are sending on the backend */ 
+axios.defaults.withCredentials = true;
+
+ export async function onRegistration(registrationData) {
+    return await axios.post('http://localhost:8000/api/register',
+        registrationData
+    )
+ }
+
+ export async function onLogin(loginData) {
+    return await axios.post ('http://localhost/api/login', loginData)
+ }
+
+ export async function onLogout() {
+    return await axios.get ('http://localhost/api/logout')
+ }
+
+ export async function fetchProtectedInfo() {
+    return await axios.get ('http://localhost/api/protected')
+ }
