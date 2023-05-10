a71f609d9d2a7526f79cd43c5dd2cfa7eaf0cf40
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-10 21:03:46 +0100

    Creating useState and functions for pages, register component in pern app

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,12 +1,58 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React, { useState } from "react";
+import Layout from "../components/layout";
 
 const Register = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const onChange = () => {};
+
+  const onSubmit = (e) => {
+    e.preventDefault(); //prevents a refresh of page when we submit the form
+  };
+
   return (
     <Layout>
+      <form onSubmit={(e) => onSubmit(e)} className="container mt-3"></form>
       <h1>Register</h1>
-    </Layout>
-  )
-}
 
-export default Register
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email address
+        </label>
+        <input
+          onChange={(e) => onChange(e)}
+          type="email"
+          className="form-control"
+          id="email"
+          name="email"
+          value={values.email}
+          placeholder="test@gmail.com"
+          required //field required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input 
+        onChange={(e) => onChange(e)}
+        type="text"
+        className="form-control"
+        id="password"
+        name="password"
+        value={values.password}
+        placeholder="password"
+        required //field required
+        />
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
