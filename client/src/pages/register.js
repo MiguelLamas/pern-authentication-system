5cc32728bb58d03ff1b20a0ed3bb00334aab8323
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-10 23:32:59 +0100

    Finished working on the functionality of Register component

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { onRegistration } from "../api/auth";
 import Layout from "../components/layout";
 
 const Register = () => {
@@ -9,10 +10,23 @@ const Register = () => {
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
 
-  const onChange = () => {};
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault(); //prevents a refresh of page when we submit the form
+
+    try {
+      const { data } = await onRegistration(values);
+
+      setError("");
+      setSuccess(data.message);
+      setValues({ email: "", password: "" });
+    } catch (error) {
+      setError(error.response.data.errors[0].msg);
+      setSuccess("");
+    }
   };
 
   return (
@@ -36,7 +50,7 @@ const Register = () => {
           />
         </div>
 
-        <div>
+        <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
@@ -51,6 +65,10 @@ const Register = () => {
             required //field required
           />
         </div>
+
+        <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
+        <div style={{ color: "green", margin: "10px 0" }}>{success}</div>
+
 
         <button type="submit" className="btn btn-primary">
           Submit
